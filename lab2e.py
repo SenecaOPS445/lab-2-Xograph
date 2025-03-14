ebda7d1a8911d8946ef517efd9dc846b5ca8f87a
Kevin Ho <kho55@myseneca.ca>
2025-03-14 14:11:15 -0400

    completed lab

---
+++ lab2e.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''Author: Kevin Ho'''
+'''Author ID: 100797190'''
+'''Date Created: 2025/03/03'''
+
+timer = 10
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print('blast off!')
---
+++ lab2f.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''Author: Kevin Ho'''
+'''Author ID: 100797190'''
+'''Date Created: 2025/03/03'''
+import sys
+
+timer = int(sys.argv[1])
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print('blast off!')
